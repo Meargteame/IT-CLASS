875c75235a1b3055674cac57b1920383cc562eb8
Meargteame <mearegteame99995555@gmail.com>
2025-03-17 00:42:25 +0300

    Methods in c#

---
+++ event_programming_with_c#/c#_dev/console_app/Methods.cs
@@ -0,0 +1,38 @@
+// Methods 
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello C# Compiler");
+        string user = "john";
+        GreetUser(user);
+        GreetUser2(user);
+
+        sayHi(user);
+
+    }
+
+    public int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+
+    // parameters => required , optional .variable-length parameters 
+    public static void GreetUser(string user) // required 
+    {
+        Console.WriteLine("Hello , " + user);
+    }
+    public static void GreetUser2(string user = "Unknown") // optional 
+    {
+        Console.WriteLine("Hello ," + user);
+    }
+
+    public static void sayHi(string name)
+    {
+        Console.WriteLine("Hi, " + name);
+        return;
+    }
+
+
+}
