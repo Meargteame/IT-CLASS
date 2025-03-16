de47af1f6e45d7c5d4e78479b873e6a12b775383
Meargteame <mearegteame99995555@gmail.com>
2025-03-16 15:22:05 +0300

    C# type conversion

---
+++ event_programming_with_c#/c#_dev/app_1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        // Parse / Conversion 
+        string textBoxAge = "jon";
+
+        // var age = int.Parse(textBoxAge);
+        // age = bool.Parse(textBoxAge);
+        // age = double.Parse(textBoxAge);
+        // age = DateTime.Parse(textBoxAge);
+
+
+        // Console.WriteLine(age);
+
+        bool tryParseResult = int.TryParse(textBoxAge, out int age);
+
+        if (!tryParseResult)
+        {
+            Console.WriteLine(age);
+            Console.WriteLine("Invalid Input");
+        }
+
+        Convert.
+    }
+}
